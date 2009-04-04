a0cfc7b9b0fd8ceb0f0127a5119f84479d44f0d3
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2009-04-04 14:31:19 +0000

    Now building the man pages properly.

--- pthreads/Common.cmake
+++ pthreads/Common.cmake
@@ -1,3 +1,39 @@
+INCLUDE(FindPerl)
+IF (NOT PERL_FOUND)
+    MESSAGE ( FATAL_ERROR "perl must be installed")
+ENDIF(NOT PERL_FOUND)
+
 MACRO(CHOMP VAR)
     STRING(REGEX REPLACE "[\r\n]+$" "" ${VAR} "${${VAR}}")
 ENDMACRO(CHOMP)
+
+MACRO(RUN_POD2MAN SOURCE DEST SECTION CENTER RELEASE)
+    SET(PATH_PERL ${PERL_EXECUTABLE})
+    ADD_CUSTOM_COMMAND(
+        OUTPUT ${DEST}
+        COMMAND ${PATH_PERL}
+        ARGS "-e"
+        "my (\$src, \$dest, \$sect, \$center, \$release) = @ARGV; my \$pod = qq{Hoola.pod}; use File::Copy; copy(\$src, \$pod); system(qq{pod2man --section=\$sect --center=\"\$center\" --release=\"\$release\" \$pod > \$dest}); unlink(\$pod)"
+        ${SOURCE}
+        ${DEST}
+        ${SECTION}
+        "${CENTER}"
+        "${RELEASE}"
+        MAIN_DEPENDENCY ${SOURCE}
+        VERBATIM
+    )
+    ADD_CUSTOM_TARGET(
+        POD_${DEST} ALL
+        DEPENDS ${DEST}
+    )
+ENDMACRO(RUN_POD2MAN)
+
+MACRO(INSTALL_MAN SOURCE SECTION)
+    INSTALL(
+        FILES
+            ${SOURCE}
+        DESTINATION
+            "share/man/man${SECTION}"
+   )
+ENDMACRO(INSTALL_MAN)
+
